05b0542d29b6225f70b1f3727df1010310572dcd
Gopika-Dileep <gopikadileep76@gmail.com>
2026-03-13 16:02:00 +0530

    feat: Implement company repository interface and class for company management

---
+++ Server/src/interfaces/repositories/ICompanyRepository.ts
@@ -0,0 +1,8 @@
+import { ICompany } from "../../models/company.model";
+
+
+export interface ICompanyRepository{
+    createCompany(userId:string,companyName:String):Promise<ICompany>
+    findCompanyByUserId(userId:string):Promise<ICompany|null>
+
+}
