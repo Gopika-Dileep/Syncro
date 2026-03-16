d5472c6da172bda826aae077659f32f3ac9f5336
Gopika-Dileep <gopikadileep76@gmail.com>
2026-03-16 21:14:20 +0530

    feat: Redesign Company and Employee layouts with a new Header component for improved navigation and user experience

--- Client/src/layouts/EmployeeLayout.tsx
+++ Client/src/layouts/EmployeeLayout.tsx
@@ -1,14 +1,18 @@
 import EmployeeSidebar from "@/components/employee/EmployeeSidebar"
+import Header from "@/components/Header"
 import { Outlet } from "react-router-dom"
 
 
 export default function EmployeeLayout() {
     return (
-        <div style={{ display: "flex" }}>
+        <div className="flex bg-gray-50 min-h-screen">
             <EmployeeSidebar />
-            <main style={{ flex: 1, padding: "16px" }}>
-                <Outlet />     {/* child page renders here */}
-            </main>
+            <div className="flex-1 flex flex-col">
+                <Header />
+                <main className="flex-1 p-6 overflow-auto">
+                    <Outlet />     {/* child page renders here */}
+                </main>
+            </div>
         </div>
     )
 }
